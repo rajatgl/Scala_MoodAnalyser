020424d6ae6f49229d7aa7ce500ac91908478a68
Rajat Gundi <glrajat@gmail.com>
2020-11-19 14:25:49 +0530

    Added: AnalyseMood method

--- src/main/scala/com/bridgelabz/moodanalyser/MoodAnalyser.scala
+++ src/main/scala/com/bridgelabz/moodanalyser/MoodAnalyser.scala
@@ -6,5 +6,13 @@ package com.bridgelabz.moodanalyser
  * Author: Rajat G.L.
  */
 object MoodAnalyser {
-  
+  def moodAnalyser(message: String): String = {
+    if (message.contains("Sad"))
+      "SAD"
+    else
+      "HAPPY"
+  }
+  def main(args: Array[String]): Unit = {
+    println(moodAnalyser("I'm happy today"))
+  }
 }
